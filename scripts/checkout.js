2b29285caaaabfac3f4ed0cd20d8a3c855e2e337
BanjoTech <banjoyinka0022@gmail.com>
2025-05-27 09:48:23 +0100

    date js external library

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,11 @@ import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
-hello();
-console.log(dayjs());
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+console.log(deliveryDate.format('dddd, mmmm D'));
+
+
 
 let cartSummaryHTML = '';
 
