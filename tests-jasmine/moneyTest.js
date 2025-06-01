bca256d478a5685d9fbc616813952a5dd234d87a
BanjoTech <banjoyinka0022@gmail.com>
2025-06-01 21:36:45 +0100

    update

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,19 @@
+import { formatCurrency } from '../scripts/utils/money.js';
+
+describe('test suite: formatCurrency', () => {
+  it('converts cents into dollars', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+
+  it('works with 0', () => {
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+
+  it('rounds up to the nearest cents', () => {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
+
+  it('also rounds up to the nearest cents but result must be failed for it to be regarded as passed', () => {
+    expect(formatCurrency(2000.4)).toEqual('20.01');
+  });
+});
