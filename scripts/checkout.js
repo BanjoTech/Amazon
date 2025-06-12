d7a05a4cd43f90ab073a23d4002b258bab09da06
BanjoTech <banjoyinka0022@gmail.com>
2025-06-13 00:23:36 +0100

    learnt promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,16 +5,40 @@ import {loadCart} from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) => {
+   loadCart(() => {
+    resolve('value2');
+   });
+  })
+]).then((values) => {
+  console.log(values);
+  renderOrderSummary();
+  renderPaymentSummary();
+});
 
+/*
 new Promise((resolve) => {
   loadProducts(() => {
+    resolve('value1');
+  });
+}).then((value) => {
+  console.log(value);
+  return new Promise((resolve) => {
+   loadCart(() => {
     resolve();
+   });
   });
 }).then(() => {
-  return new Promise();
-  loadCart();
-})
-
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
 
 
 /*
